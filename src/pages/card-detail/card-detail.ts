9bd29220368143db93de9aa8218d5b586f55d7eb
jordanc44 <clark.jordan@outlook.com>
2018-04-23 17:30:56 -0500

    Sprint 1
    Allergen preferences, menu item display with firebase

--- src/pages/card-detail/card-detail.ts
+++ src/pages/card-detail/card-detail.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, ModalController, NavParams } from 'ionic-angular';
-import { AngularFireDatabase } from 'angularfire2/database'; 
+import { AngularFireDatabase, AngularFireAction } from 'angularfire2/database'; 
 import { Observable } from 'rxjs/Observable';
 import { Item } from '../../models/item';
 import { Items } from '../../providers/providers';
 import { Reviews } from '../../providers/providers';
+import { DataSnapshot } from '@firebase/database';
+import { HttpClient } from '@angular/common/http';
+import { AllegensProvider} from '../../providers/allegens/allegens';
+
+
 
 
 @IonicPage()
@@ -17,27 +22,62 @@ import { Reviews } from '../../providers/providers';
 export class CardDetailPage {
   item: any;
   review: any;
+  show: boolean[];
+  
+ 
+
 
   currentReviews: Item[];
   reviewsObservable: Observable<any[]>;
+  menuTypes: Observable<any[]>;
+  information: any[];
+  
 
-  constructor(public navCtrl: NavController,  private db: AngularFireDatabase, navParams: NavParams, public reviews: Reviews, public items: Items, public modalCtrl: ModalController) {
-    this.item = navParams.get('item') || reviews.defaultReview;
+  constructor(public allergies :AllegensProvider, private http: HttpClient, public navCtrl: NavController,  private db: AngularFireDatabase, navParams: NavParams, public reviews: Reviews, public items: Items, public modalCtrl: ModalController) {
+   this.item = navParams.get('item') || reviews.defaultReview;
     this.currentReviews = this.reviews.query();
-
+    
     
   }
 
+
+   
   ngOnInit() {
-    console.log('/' + this.item.data + '/reviews');
-    this.reviewsObservable = this.getReviews('/' + this.item.data + '/reviews');
+    this.show = [false, false, false];
+    this.menuTypes = this.getReviews('/' + this.item.data );
+    console.log(this.allergies.wheat);
+  /*  this.db.database.ref("/lous").on("value", function(snapshot) {
+      console.log(snapshot.val());
+    });
+
+*/
     
-    
+
   }
+
+  shower(i){
+    this.show[i] = !this.show[i];
+  }
+
+
+
+ 
+  toggleSection(i) {
+   
+    this.information[i].open = !this.information[i].open;
+  }
+ 
+
+
 
   getReviews(listPath): Observable<any[]> {
+
+ 
     return this.db.list(listPath).valueChanges();
+    
+
   }
+  
 
   getCard()
   {
