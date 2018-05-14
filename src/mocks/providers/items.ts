70051963703ed89daafcd438472388d32c011497
jordanc44 <clark.jordan@outlook.com>
2018-05-14 18:16:04 -0500

    Sprint 5
    Added restaurants

--- src/mocks/providers/items.ts
+++ src/mocks/providers/items.ts
@@ -18,16 +18,19 @@ export class Items {
         "lat": 42.0514654,
         "lng": -87.6820016,
         "index": 0,
+        "number" : "tel:+18473285400"
       },
       {
         "name": "Chipotle",
-        "profilePic": "http://teacherpress.ocps.net/hilights/files/2018/01/20170707_GH_CHIPOTLE_1010-1021x580.jpg",
+        "profilePic": "https://preview.ibb.co/kFrhm7/chip.jpg",
         "about": "Fast-food chain offering Mexican fare, including design-your-own burritos, tacos & bowls.",
         "data": "chipotle",
         "address": "711 Church St, Evanston, IL 60201",
         "lat": 42.048613,
         "lng": -87.681222,
-        "index": 1
+        "index": 1,
+        "number": "tel:+18474253959"
+
       },
       {
         "name": "La Cocinita",
@@ -37,7 +40,8 @@ export class Items {
         "address": "1625 Chicago Ave, Evanston, IL 60201",
         "lat": 42.047046,
         "lng": -87.678949,
-        "index": 2
+        "index": 2,
+        "number": "tel:+18473321625"
       },
       {
         "name": "Dave's New Kitchen",
@@ -47,7 +51,8 @@ export class Items {
         "address": "815 Noyes St, Evanston, IL 60201",
         "lat": 42.058594,
         "lng": -87.682534,
-        "index": 3
+        "index": 3,
+        "number": "tel:+18478646000"
       },
       {
         "name": "Aloha Poke",
@@ -57,37 +62,52 @@ export class Items {
         "address": "630 Davis St, Evanston, IL 60201",
         "lat": 42.046513,
         "lng": -87.681215,
-        "index": 4
+        "index": 4,
+        "number": "tel:+12249997298"
       },
       {
         "name": "Naf Naf Grill",
-        "profilePic": "https://www.qsrmagazine.com/sites/qsrmagazine.com/files/styles/story_page/public/story/one-watch-naf-naf-grill.jpg",
+        "profilePic": "https://www.minneapolisnorthwest.com/images/made/images/remote/https_minneapolis-nw.s3.amazonaws.com/CMS/2279/naf_naf_600_400_50auto.jpg",
         "about": "Modern Middle Eastern counter-serve chain dishing up shawarma & falafel on pitas, rice or salads.",
         "data": "nafnaf",
         "address": "1629 Orrington Ave, Evanston, IL 60201",
         "lat": 42.047687,
         "lng": -87.680707,
-        "index": 5
+        "index": 5,
+        "number": "tel:+18478668470"
       },
       {
         "name": "Blaze Pizza",
-        "profilePic": "http://4e3tuo3do51baai522lhhji4.wpengine.netdna-cdn.com/wp-content/uploads/2016/11/blazepizza_81-600x399.jpg",
+        "profilePic": "https://image.ibb.co/kgE4Yn/blaze.jpg",
         "about": "Hip counter-serve pizzeria dishing up crispy, thin-crust pies made with creative toppings & sauces.",
         "data": "blaze",
         "address": "1737 Sherman Ave, Evanston, IL 60201",
         "lat": 42.049606,
         "lng": -87.681572,
-        "index": 6
+        "index": 6,
+        "number": "tel:+18472649263"
       },
       {
         "name": "Frontera Fresco",
-        "profilePic": "http://www.rickbayless.com/wp-content/uploads/2015/05/RB.com_FrescoMenuItem_11.jpg",
+        "profilePic": "https://www.rickbayless.com/wp-content/uploads/2015/05/RB.com_FrescoMenuItem_11.jpg",
         "about": "Quick-serve outpost offering market-driven Mexican favorites plus fruit juices.",
         "data": "frontera",
         "address": "1999 Campus Dr, Evanston, IL 60208",
         "lat": 42.053309,
         "lng": -87.672955,
-        "index": 7
+        "index": 7,
+        "number": "tel:+18474912300"
+      },
+      {
+        "name": "Boltwood",
+        "profilePic": "https://media2.fdncms.com/chicago/imager/a-salad-of-roasted-beets-spiced-pistachio/u/magnum/14472201/beetsgrapefruit-magnum.jpg",
+        "about": "Stylishly simple, window-filled space for farm-to-table New American fare & craft cocktails.",
+        "data": "boltwood",
+        "address": "804 Davis St, Evanston, IL 60201",
+        "lat": 42.046666,
+        "lng": -87.682288,
+        "index": 8,
+        "number": "tel:+18478592880"
       },
 
      
